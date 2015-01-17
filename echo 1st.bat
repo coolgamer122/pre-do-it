ba10a92302d95ab4819093d4e2728b7715109e09
coolgamer122 <edisondieu@gmail.com>
2015-01-16 16:49:32 -0800

    Create echo 1st.bat

---
+++ echo 1st.bat
@@ -0,0 +1,19 @@
+//do not do any thing//
+@echo off
+echo hello
+pause
+echo welcome to my echo
+pause
+echo here are xsample
+echo hello ...
+echo 1
+echo 2 
+pause
+echo.
+echo so here is a game sutup
+echo if%sample%== sample goto 1
+echo if%sample%== sample goto 2
+echo.
+puse
+echo so that is how you do it
+pause
